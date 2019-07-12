4294794f20fb0a55d2bed41f690ec27821528053
Guillermo Campos <guillermo.campos@usach.cl>
2019-07-12 13:42:03 -0400

    Hermoso Dropzone

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="home">
+    <vueDropzone id="dropzone" name="" :options ="dropzoneOptions"/>
+    <v-btn color="success" @click="handleFileUpload()">Subir</v-btn>
     <v-container grid-list-md text-xs-center>
       <v-layout row wrap>
         <v-flex v-for="proposal in proposalList" :key="proposal.id" sm12 xs12 lg4>
@@ -13,22 +15,42 @@
 <script>
 import { mapState, mapMutations, mapActions } from "vuex";
 import Proposals from '@/components/home/proposals'
+import vue2Dropzone from 'vue2-dropzone'
+import 'vue2-dropzone/dist/vue2Dropzone.min.css'
 
 export default {
   name: 'home',
   components: {
-    'home-proposals': Proposals
+    'home-proposals': Proposals,
+    vueDropzone: vue2Dropzone
+  },
+  data: function () {
+    return {
+      files: [],
+      dropzoneOptions: {
+          url: 'https://httpbin.org/post',
+          thumbnailWidth: 150,
+          maxFilesize: 256,
+        //   headers: { "My-Awesome-Header": "header value" },
+          acceptedFiles: ".pdf",
+          //renameFile: proposal._id
+      }
+    }
   },
   created() {
     this.getAllProposals()
   },
+  mounted() {
+    var dropzoneVue = document.getElementById("dropzone").dropzone;
+    dropzoneVue.on("addedfiles", function(files) {
+      console.log(files);
+    });
+  },
   computed: {
-    ...mapState(["proposalList"])
+    ...mapState(["proposalList"]),
   },
   methods: {
-    ...mapActions(["getAllProposals"]),
+    ...mapActions(["getAllProposals"])
   }
 }
 </script>
-
-
