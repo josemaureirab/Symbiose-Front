dc9b7b7dae24833773fed2baa35f41ff8d5462de
Guillermo Campos <guillermo.campos@usach.cl>
2019-07-17 23:58:48 -0400

    Online

--- src/store.js
+++ src/store.js
@@ -19,8 +19,8 @@ export default new Vuex.Store({
     },
     items: [],
     /* General Stuffs */
-    serverURL: 'http://localhost:9000',
-    //serverURL: 'http://projecthunter.tk:8080/symbiose',
+    //serverURL: 'http://localhost:9000',
+    serverURL: 'http://projecthunter.tk:8080/symbiose',
     proposalList: [],
     clientsList: [],
     usersList: [],
